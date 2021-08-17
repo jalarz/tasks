351db4adcd1a542c7574539367a9a2fdc6a8f0fa
rzjala <jalanr@code.edu.az>
2021-08-17 10:07:04 +0400

    task

---
+++ class task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace class_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(sum(arr));
+        }
+         static int sum(int[] arr)
+        {
+            int summ = 0;
+           int average = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                summ += arr[i];   
+            }
+            average = summ / arr.Length;
+
+            return average;
+        }
+        
+}
+    }
+
